7fa5a9f74fd1c6c086a3f933a1d7274a552edf2c
Braxton Chatman <braxton.a.chatman@gmail.com>
2023-07-25 16:29:41 -0700

    format and add command function for 'generate' button

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,7 +3,7 @@ from random import choice, randint
 import string
 
 
-def generate_password(upper=8, lower=8, let_lower=True, let_upper=True, let_nums=True, let_special=True):
+def generate_password(upper=8, lower=8, let_upper=True, let_lower=True, let_nums=True, let_special=True):
     """Generates a random password according to given parameters
     
     upper and lower inputs are the length limits for the password
@@ -12,16 +12,16 @@ def generate_password(upper=8, lower=8, let_lower=True, let_upper=True, let_nums
 
     available_lists = []
     pass_list = []
-
-    # if selected add lowercase letters to selection list and add one lowercase digit to password
-    if let_lower:
-        available_lists.append(string.ascii_lowercase)
-        pass_list.append(choice(string.ascii_lowercase))
         
     # if selected add uppercase letters to selection list and add one uppercase digit to password
     if let_upper:
         available_lists.append(string.ascii_uppercase)
         pass_list.append(choice(string.ascii_uppercase))
+
+    # if selected add lowercase letters to selection list and add one lowercase digit to password
+    if let_lower:
+        available_lists.append(string.ascii_lowercase)
+        pass_list.append(choice(string.ascii_lowercase))
     
     # if selected add numbers  to selection list and add one digit to password
     if let_nums:
@@ -32,6 +32,10 @@ def generate_password(upper=8, lower=8, let_lower=True, let_upper=True, let_nums
     if let_special:
         available_lists.append(string.punctuation)
         pass_list.append(choice(string.punctuation))
+
+    # return empty string if no character types selected
+    if not available_lists:
+        return ""
 
     # pick a random digit from one of the available lists and add to pass list
     pass_length = randint(lower, upper)
@@ -110,17 +114,74 @@ def validate_password(password, length=8, need_upper=True, need_lower=True, need
     return out_list
 
 
+def button_generate(upper, lower, let_upper, let_lower, let_nums, let_special):
+    """Gets the tkinter variables corresponding to user selected
+    password requirements and inputs them to generate_password
+    function to return new password"""
+
+    global out_space_var
+
+    have_upper = let_upper.get()
+    have_lower = let_lower.get()
+    have_nums = let_nums.get()
+    have_special = let_special.get()
+    upper_lim = int(upper.get())
+    lower_lim = int(lower.get())
+
+    new_password = generate_password(upper = upper_lim, lower = lower_lim, let_upper = have_upper, let_lower = have_lower, let_nums = have_nums, let_special = have_special)
+    print(new_password)
+    out_space_var.set(new_password)
+
+
+def main():
+    # main window
+    root = tk.Tk()
+    root.geometry("400x250")
+    root.title("Password Generator")
+
+    frame = tk.Frame(root)
+    frame.pack()
+
+    # labelframe for required characters
+    char_selection_frame = tk.LabelFrame(frame, text = "Select Required Characters")
+    char_selection_frame.grid(row = 0, column = 0)
+
+    # checkbutton selection for required characters
+    upper_var = tk.BooleanVar(value = False)
+    lower_var = tk.BooleanVar(value = False)
+    num_var = tk.BooleanVar(value = False)
+    special_var = tk.BooleanVar(value = False)
+
+    upper_check = tk.Checkbutton(char_selection_frame, text = "Include Uppercase Letters", variable = upper_var, onvalue = True, offvalue = False)
+    lower_check = tk.Checkbutton(char_selection_frame, text = "Include Lowercase Letters", variable = lower_var, onvalue = True, offvalue = False)
+    num_check = tk.Checkbutton(char_selection_frame, text = "Include Numbers", variable = num_var, onvalue = True, offvalue = False)
+    special_check = tk.Checkbutton(char_selection_frame, text = "Include Special Characters", variable = special_var, onvalue = True, offvalue = False)
+
+    upper_check.grid(row = 0, column = 0, padx = 5)
+    lower_check.grid(row = 1, column = 0, padx = 5)
+    num_check.grid(row = 0, column = 1, sticky = "w", padx = 5)
+    special_check.grid(row = 1, column = 1, padx = 5)
+
+    # password length spinbox
+    length_spin = tk.Spinbox(char_selection_frame, from_ = 4, to = 50, width = 50, state = 'readonly')
+    length_spin.grid(row = 2, column = 0, columnspan = 2, pady = 10)
+
+    # output entry space
+    out_frame = tk.LabelFrame(frame, text = "New Password:", width = 200)
+    out_frame.grid(row = 1, column = 0, sticky = "ew", pady = 10)
     
+    global out_space_var
+    out_space_var = tk.StringVar()
+    out_space = tk.Entry(out_frame, width = 50, textvariable = out_space_var)
+    out_space.pack(pady = 5)
+    
+    # generate password button
+    gen_butt = tk.Button(out_frame, text = "Generate", command = lambda: button_generate(length_spin, length_spin, upper_var, lower_var, num_var, special_var))
+    gen_butt.pack(pady = 10)
 
 
-
-# root = tk.Tk()
-# root.title("Password Generator")
+    root.mainloop()
 
 
-
-
-
-
-
-# root.mainloop()
+if __name__ == "__main__":
+    main()
